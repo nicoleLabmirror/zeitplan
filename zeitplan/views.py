8b33f60f3e0851ace7e50b440cd88abdf7d65533
kahliLab <klaus.kahlfuss@kstp.at>
2022-08-04 19:36:44 +0200

    New text (context) für view "index"

--- zeitplan/views.py
+++ zeitplan/views.py
@@ -4,7 +4,7 @@ from .models import Day
 
 
 def index(request):
-    text = 'Hallo, das ist der aktuelle Prototyp der App "Zeitplan ... in progress"'
+    text = 'Hallo, das ist der aktuelle Prototyp der App "Zeitplan ... in progress ... still"'
     context = {"text": text}
     return render(request, "zeitplan/index.html", context)
 
