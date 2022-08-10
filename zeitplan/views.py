3c4cf4e41fa3ad36710e0eaaa4453234c797e771
kahliLab <klaus.kahlfuss@kstp.at>
2022-08-10 21:16:37 +0200

    Clean (and understandable) structure in views

--- zeitplan/views.py
+++ zeitplan/views.py
@@ -12,28 +12,27 @@ def index(request):
 def overview(request):
     all_days_list = Day.objects.all().order_by("day_date")
     context = {"all_days_list": all_days_list}
+
     return render(request, "zeitplan/overview.html", context)
 
 def day_overview(request, day_id):
-    time_entry_list = Time_entry.objects.filter(
-        day=day_id
-    )
+    day = Day.objects.get(pk=day_id)
+    time_entry_list = day.time_entry_set.all()
     context = {
+        "day": day,
         "time_entry_list": time_entry_list,
-        "day_id": day_id
     }
+
     return render(request, "zeitplan/day_overview.html", context)
 
 def day_edit(request, day_id):
-    time_entry_list = Time_entry.objects.filter(
-        day=day_id
-    )
     day = Day.objects.get(pk=day_id)
+    time_entry_list = day.time_entry_set.all()
     context = {
+        "day": day,
         "time_entry_list": time_entry_list,
-        "day_id": day_id,
-        "day_name": day.day_name,
     }
+
     return render(request, "zeitplan/day_editing.html", context)
 
 def day_votes(request, day_id):
@@ -42,16 +41,18 @@ def day_votes(request, day_id):
         entries = day.time_entry_set.get(pk=request.POST["entry"])
     except (KeyError, Day.DoesNotExist):
         context = {
-            "day_id": day_id,
+            "day": day,
             "error message": "Why?"
         }
+
         return render(request, "zeitplan/day_editing.html", context)
     else:
         entries.votes += 1
         entries.save()
+
         return HttpResponseRedirect(
             reverse(
                 "zeitplan:day_overview",
-                args=(day_id,)
+                args=(day.id,)
             )
         )
