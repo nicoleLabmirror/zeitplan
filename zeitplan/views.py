0f47ee50305950d587a4466da6807b29132341c3
kahliLab <klaus.kahlfuss@kstp.at>
2022-08-12 20:41:07 +0200

    "day_votes" view now can handle a additional/second button

--- zeitplan/views.py
+++ zeitplan/views.py
@@ -45,11 +45,23 @@ def day_edit(request, day_id):
 def day_votes(request, day_id):
     day = Day.objects.get(pk=day_id)
     try:
-        entries = day.time_entry_set.get(pk=request.POST["entry"])
+        if "entry_vote" in request.POST:
+            print("VOTE")
+            entries_votes = day.time_entry_set.get(pk=request.POST["entry_vote"])
+        elif "entry_passed" in request.POST:
+            print("PASSED")
+            entries_passed = day.time_entry_set.get(pk=request.POST["entry_passed"])
     except (KeyError, Day.DoesNotExist):
         context = {"day": day, "error message": "Why?"}
         return render(request, "zeitplan/day_editing.html", context)
     else:
-        entries.votes += 1
-        entries.save()
+        if "entry_vote" in request.POST:
+            entries_votes.votes += 1
+            entries_votes.save()
+        elif "entry_passed" in request.POST:
+            if entries_passed.entry_passed is None:
+                entries_passed.entry_passed = True
+            else:
+                entries_passed.entry_passed = None
+            entries_passed.save()
         return HttpResponseRedirect(reverse("zeitplan:day_overview", args=(day.id,)))
