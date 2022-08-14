d5587798d821823f057108381fcd2a0ee4e339a0
kahliLab <klaus.kahlfuss@kstp.at>
2022-08-14 10:43:11 +0200

    Add "day_delete" view and path for un urls.py

--- zeitplan/urls.py
+++ zeitplan/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("newday/", views.day_new, name="day_new"),
     path("addnewday/", views.day_add_new, name="day_add_new"),
     path("<int:day_id>/", views.day_overview, name="day_overview"),
+    path("<int:day_id>/delete/", views.day_delete, name="day_delete"),
     path("<int:day_id>/edit/", views.day_edit, name="day_edit"),
     path("<int:day_id>/votes/", views.day_votes, name="day_votes"),
 ]
