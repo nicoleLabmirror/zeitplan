ccce0052fa96a726f53cc618db206c9f31db8691
kahliLab <klaus.kahlfuss@kstp.at>
2022-08-16 21:34:43 +0200

    Add path for new view "add_time_entry"

--- zeitplan/urls.py
+++ zeitplan/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path("<int:day_id>/votes/", views.day_editing, name="day_editing"),
     path("<int:day_id>/new_time_entry/", views.time_entry_add, name="time_entry_add"),
     path("<int:entry_id>/category_add/", views.category_add, name="category_add"),
+    path("<int:entry_id>/add_time_frame/", views.add_time_frame, name="add_time_frame"),
 ]
