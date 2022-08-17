ab64ba7b164be8046c17139e193e1cdc054d42ad
kahliLab <klaus.kahlfuss@kstp.at>
2022-08-17 21:03:14 +0200

    Repalce own_modules.py by helper_functions.py

---
+++ zeitplan/helper_functions.py
@@ -0,0 +1,12 @@
+def get_german_day_name(english_day_name):
+    german_translation_for_day_name = {
+        "Monday": "Montag",
+        "Tuesday": "Dienstag",
+        "Wednesday": "Mittwoch",
+        "Thursday": "Donnerstag",
+        "Friday": "Freitag",
+        "Saturday": "Samstag",
+        "Sunday": "Sonntag",
+    }
+    german_day_name = german_translation_for_day_name[english_day_name]
+    return german_day_name
