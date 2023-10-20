9f7f021a5249b168e8f09b188a1d79f00ffa5638
nicoleLab <klaus.kahlfuss@kstp.at>
2023-10-20 22:07:22 +0200

    Add django-htmx in settings

--- urlaub/settings.py
+++ urlaub/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_bootstrap5",
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "urlaub.urls"
